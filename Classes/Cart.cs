30e8f903843e45ddb9441c83e85abf60580e0ad9
david-eta <deta@email.sc.edu>
2023-10-22 09:44:08 -0400

    add to and remove from cart

--- Classes/Cart.cs
+++ Classes/Cart.cs
@@ -8,9 +8,50 @@
 public class Cart
 {
     public int CartID { get; set; }
-    public Dictionary<Item, int>? Items { get; set; }
+    public Dictionary<CartItem, int>? Items { get; set; }
     public Cart(int cartID)
     {
         CartID = cartID;
+        Items = new Dictionary<CartItem, int>();
+    }
+
+    // Add an item to the cart
+    public void AddItem(Item item, int quantity)
+    {
+        if (item != null && quantity > 0)
+        {
+            CartItem cartItem = new CartItem { Item = item, Quantity = quantity };
+
+            if (Items.ContainsKey(cartItem))
+            {
+                Items[cartItem] += quantity;
+            }
+            else
+            {
+                Items.Add(cartItem, quantity);
+            }
+        }
+    }
+
+    // Remove an item from the cart
+    public void RemoveItem(Item item, int quantity)
+    {
+        if (item != null && quantity > 0)
+        {
+            CartItem cartItem = new CartItem { Item = item, Quantity = 0 };
+
+            if (Items.ContainsKey(cartItem))
+            {
+                int currentQuantity = Items[cartItem];
+                if (currentQuantity > quantity)
+                {
+                    Items[cartItem] -= quantity;
+                }
+                else
+                {
+                    Items.Remove(cartItem);
+                }
+            }
+        }
     }
 }
