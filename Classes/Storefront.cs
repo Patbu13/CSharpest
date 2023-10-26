093b5db96e34127577fc4be5ceed452b288f04cf
vivianofsouza <vdsouza@email.sc.edu>
2023-10-25 20:57:17 -0400

    added business logic to Checkout Facade Class, made Storefront Singleton

--- Classes/Storefront.cs
+++ Classes/Storefront.cs
@@ -5,11 +5,10 @@
 //	Last Updated : 10/12/23
 
 //  Interaction with the main page of the storefront (not sure if will need)
-public class Storefront
-{
-    public Dictionary<Item, int> Stock {  get; set; }
-    public Storefront()
-    {
 
-    }
+// Make this a singleton. 
+public static class Storefront
+{
+    public static Dictionary<Item, int> Stock {  get; set; }
+
 }
