b4e283a8ff86aecf464f2e341cd21ad85f60f765
Patbu13 <patrickbu_SC@hotmail.com>
2023-10-29 13:48:21 -0400

    moved checkout functions to checkout controller

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -12,13 +12,59 @@ namespace CSharpest
         [ApiController]
         public class CheckoutController : ControllerBase
         {
-   
-            // POST: <CheckoutController>/card
+            
+            // takes in a new card and then saves it to user object
+            // POST: <CheckoutController>/address
             [HttpPost("{card}")]
+            public bool takeCardInput(Card card, User user)
+            {
+                if (ValidateCardDetails(card))
+                {
+                    // if card validation successful, save to user's list of cards
+                    if (!(user.UserCards.Contains(card)))
+                    {
+                        user.UserCards.Add(card);
+                    }
+                    // otherwise, validation is still successful
+                    return true;
+                }
+
+                // if not valid, returns false
+                return false;
+            }
+            
+
+            // POST: <CheckoutController>/card
+            [HttpPost("{cardCheck}")]
             public bool ValidateCardDetails(Card card)
             {
+                // checks for valid credit card number length
+                if (card.Number.ToString().Length > 19 || card.Number.ToString().Length < 16)
+                {
+                    return false;
+                }
+
+                // checks for valid month
+
+                if (card.Month < 1 || card.Month > 12)
+                {
+                    return false;
+                }
+
+                // checks for valid year
+
+                if (card.Year <= 2022 || card.Year > 2100)
+                {
+                    return false;
+                }
+
+                if (card.CVV.ToString().Length != 3 || card.CVV.ToString().Length != 4)
+                {
+                    return false;
+                }
                 return true;
             }
+
 
             // POST: <CheckoutController>/address
             [HttpPost("{address}")]
@@ -29,8 +75,34 @@ namespace CSharpest
 
             // POST: <CheckoutController>/purchase
             [HttpPost("{cart}")]
-            public void purchase (Cart cart)
+            public bool purchase (Cart cart)
             {
+                // reduce stock by quantity purchased
+
+                // save this transaction to user's history
+
+                return true;
+            }
+
+            [HttpPost("{total}")]
+            public decimal calculateTotal(Cart cart)
+            {
+                decimal total = 0;
+                for (int i = 0; i < cart.Items.Count; i++)
+                {
+                    KeyValuePair<Item, int> Item = cart.Items.ElementAt(i);
+
+                    // adds to total the cost of each item, times the quantity of that item
+                    total += ((Item.Key.Price) * (Item.Value));
+                }
+
+                // adds tax to total; flat rate of 8% (for now)
+                total += (total * 1.08m);
+
+                // calculate shipping; for now flat rate of 5.99
+                total += 5.99m;
+
+                return total;
             }
 
 
