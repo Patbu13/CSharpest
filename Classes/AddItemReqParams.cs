8ef97ce8d58ba26dd4ded76abe08f8d411d569ff
vivianofsouza <vdsouza@email.sc.edu>
2023-10-30 10:20:28 -0400

    fixed AddItemToCart req not working

---
+++ Classes/AddItemReqParams.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpest.Classes
+{
+    public class AddItemReqParams
+    {
+        public Guid CartID { get; set; }
+        public Guid ItemID { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
