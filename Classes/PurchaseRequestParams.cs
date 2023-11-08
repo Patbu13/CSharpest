9a57070809d98e618152797b0b7971ccc188e84d
Patbu13 <70173620+Patbu13@users.noreply.github.com>
2023-11-07 21:18:57 -0500

    Params from Phase 1 worktime

---
+++ Classes/PurchaseRequestParams.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpest.Classes
+{
+    public class PurchaseRequestParams
+    {
+        public User User { get; set; }
+        public Cart Cart { get; set; }  
+    }
+}
---
+++ Classes/RemoveItemReqParams.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpest.Classes
+{
+    public class RemoveItemReqParams
+    {
+        public Guid CartID { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
